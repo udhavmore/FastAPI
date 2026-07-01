eecbff8c09f99673ee71286ea0063dfd5a9c4b87
Udhav More <54945230+udhavmore@users.noreply.github.com>
2026-07-02 00:37:48 +0530

    Added POST call

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Ola. Migo!!"}
+
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": f"Post Created: {payload['title']} - {payload['content']}"}
